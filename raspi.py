3ab18cb3a9a5780f8783cc75ef7768519ac95023
MakDll <mattt98@hotmail.it>
2017-06-20 22:09:29 +0200

    Update raspi.py

--- raspi.py
+++ raspi.py
@@ -4,6 +4,7 @@ import random
 import datetime
 import telepot
 import RPi.GPIO as GPIO
+import MySQLdb
 
 sta = "The device is "
 tus = "off" #ultimo stato
@@ -15,6 +16,23 @@ def act(pin):
         return
 
 
+def changeme( mylist ):
+   "This changes a passed list into this function"
+   mylist = [1,2,3,4]; # This would assig new reference in mylist
+   print "Values inside the function: ", mylist
+   
+def connectionDbWithMessage(msg):
+        conn=MySQLdb.connect(host="HOST", user='USERNAME', passwd='PASSWORD', db='DATABASE') #da fixare con i parametri giusti
+
+        cursor=conn.cursor()
+        cursor.execute("codice_SQL") # Esegui la query con il parametro "msg"
+        #row=cursor.fetchone() che ti frega se devi fare insert nn vedi il valore di ritorno
+        #valore1=row[0]#
+
+        cursor.close()
+        conn.close()            
+        return  
+                
 GPIO.setmode(GPIO.BOARD) # Selettore numeratore PIN raspberry
 GPIO.setup(11, GPIO.OUT) # Selezione PIN di output
 
@@ -25,14 +43,17 @@ def handle(msg):
     print 'Got command: %s' % command
 
     if command == 'on': #comando di accensione
-       bot.sendMessage(chat_id, act(11))
-       tus='on'
+           connectionDbWithMessage("on")
+           bot.sendMessage(chat_id, act(11))
+           tus='on'
     elif command =='off': #spegnimento
-       bot.sendMessage(chat_id, act(11))
-       tus='off'
+           connectionDbWithMessage("on")
+           bot.sendMessage(chat_id, act(11))
+           tus='off'
     if command =='powstat': #stato
-       sta1= sta + tus
-       bot.sendMessage(chat_id, sta1)
+           connectionDbWithMessage("on")
+           sta1= sta + tus
+           bot.sendMessage(chat_id, sta1)
 
 bot = telepot.Bot('lololololololol')
 bot.message_loop(handle)
